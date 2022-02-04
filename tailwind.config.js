8e5348ea61fd78ff1836b28d6b978886160a2484
Tenzin Sean <tenzin.jeunesse@gmail.com>
2022-02-04 22:17:32 +0100

    add first project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+     colors: {
+      "pro-white": "#FFFF",
+      "pro-gray": "#E5E5E5",
+      "pro-background": "#2E2E2E",
+      "pro-link": "#3583F6",
+      "pro-button": "#78D48C",
+      "pro-buttons-one": "#FE9636"
+     }
+    },
+  },
+
+  fontFamily: {
+    Roboto: ['Roboto, sans-serif']
+  },
+
+  plugins: [],
+}
